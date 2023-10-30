55fdedaa4b2208407c508d476b5044596f03f140
DrakeB7738 <drake.bell@ufl.edu>
2023-10-30 18:15:36 -0400

    encoder plus main function

---
+++ main.py
@@ -0,0 +1,24 @@
+
+def encode(passcode):
+    password = ""
+    for i in passcode:
+        each_password_digit = (int(i) + 3) % 10
+        password += str(each_password_digit)
+    return password
+
+if __name__ == '__main__':
+
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        user_input = input('Please enter an option: ')
+        if user_input == "1":
+            password_input = input('Please enter your password to encode: ')
+            encoded_password = encode(password_input)
+            print('Your password has been encoded and stored!')
+
+        if user_input == "3":
+            break
