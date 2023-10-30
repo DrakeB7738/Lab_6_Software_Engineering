7753d6dc6df03dac834b82b02c793b78793a9c37
DrakeB7738 <drake.bell@ufl.edu>
2023-10-30 18:33:35 -0400

    encoder plus main function

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ def encode(passcode):
         each_password_digit = (int(i) + 3) % 10
         password += str(each_password_digit)
     return password
-
+def decode(passcode):
+    password = ""
+    for i in passcode:
+        each_password_digit = (int(i) - 3) % 10
+        password += str(each_password_digit)
+    return password
 if __name__ == '__main__':
 
     while True:
@@ -19,6 +24,8 @@ if __name__ == '__main__':
             password_input = input('Please enter your password to encode: ')
             encoded_password = encode(password_input)
             print('Your password has been encoded and stored!')
+        if user_input == "2":
+            print('The encoded password is', encoded_password,', and the original password is', password_input,'.')
 
         if user_input == "3":
             break
